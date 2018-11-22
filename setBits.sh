68698df43b6bca2471878b25af483a97551e6b05
zaderGit <lsuther@me.com>
2018-11-22 23:17:05 +0500

    Add existing file

--- setBits.sh
+++ setBits.sh
@@ -2,18 +2,14 @@
 chmod 755 setBits.sh
 
 
-d2b=$(echo "obase=2;$1" | bc)
-d2b=$(echo "$d2b" | rev)
+n=$1
 
-#for (( i = 0; i < 4; i++ )); do
-#	gpio write $i 0
-#done
 
 for (( i = 0; i < 4; i++ )); do
-	if [ "${d2b:$i:1}" == "1" ]; then
-		gpio write $i 1
-	else
-		gpio write $i 0
-	fi
+
+	a=$(echo $((n >> i)))
+	a=$(echo $((a & 1)))
+	gpio write $i $a
+
 done
 
